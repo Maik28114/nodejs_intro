832e7170579ecec61bec06f8fd6c6c71fe0e7dec
Maik28114 <bbb-maik@web.de>
2025-07-30 12:15:01 +0200

    Refactored the code to use the config file.

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
 import http from 'http';
+import { readFileSync } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
-const hostname = '127.0.0.1';
-const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const configPath = path.join(__dirname, 'config.json');
+
+const config = JSON.parse(readFileSync(configPath, 'utf8'));
+const { port, hostname } = config;
+
+
 
 const server = http.createServer((req, res) => {
     console.log(`Anfrage erhalten: ${req.method} ${req.url}`);
